292d352c8d7cb658d08aa0c03cb9aabe3f77f331
Matías Meletto <mmeletto@eest1chivilcoy.edu.ar>
2022-06-16 18:20:37 -0300

    probando codigo pero sigue sin funcionar

---
+++ BlazorApp1/js/Logica.js
@@ -0,0 +1,14 @@
+﻿var listaChori = [];
+
+function añadirChori(pcarro, pcriolla, pchimi, pcantidad, pprecio) {
+
+    var nuevoChori = {
+        carro: pcarro,
+        criolla: pcriolla,
+        chimi: pchimi,
+        cantidad: pcantidad,
+        precio: pprecio
+    };
+    console.log(nuevoChori);
+    listaChori.push(nuevoChori);
+}
---
+++ BlazorApp1/js/LogicaUsuario.js
@@ -0,0 +1,11 @@
+﻿document.query('#btnGuardar').addEventListener('click', agregarChori);
+
+function agregarChori() {
+    var sCarro = 1;
+    var sCriolla = document.querySelector('#criollak').value,
+    var sChimi = document.querySelector('#chimi').value,
+    var sCantidad = document.querySelector('#choripan').value,
+    var sPrecio = Choripan.Precio
+
+    añadirChori(sCarro,sCriolla,sChimi, sCantidad, sPrecio)
+}
