596593a3c5352f3160f8b6640710f0bfd8ebd819
Funcion de la sume de hamburguesa bien hecha <info-4@INFO4-PC5>
2022-07-01 11:39:09 -0300

    Agregado de productos

--- BlazorApp1/Data/CompraContext.cs
+++ BlazorApp1/Data/CompraContext.cs
@@ -61,10 +61,171 @@ namespace BlazorApp1.Data
                 },
                 new Producto()
                 {
+                    ProductoId = 3,
+                    CategoriaId = 3,
+                    Nombre = "Torta de chocolate",
+                    Precio = 500
+                },
+                new Producto()
+                {
+                    ProductoId = 5,
+                    CategoriaId = 3,
+                    Nombre = "Torta de zanahoria",
+                    Precio = 600
+                },
+                new Producto()
+                {
+                    ProductoId = 6,
+                    CategoriaId = 3,
+                    Nombre = "Torta de vainilla",
+                    Precio = 500
+                },
+                new Producto()
+                {
+                    ProductoId = 7,
+                    CategoriaId = 3,
+                    Nombre = "Pastafrola",
+                    Precio = 700
+                },
+                new Producto()
+                {
+                    ProductoId = 8,
+                    CategoriaId = 3,
+                    Nombre = "Brownie c/mani",
+                    Precio = 750
+                },
+                new Producto()
+                {
+                    ProductoId = 9,
+                    CategoriaId = 3,
+                    Nombre = "Brownie c/mani",
+                    Precio = 800
+                },
+                new Producto()
+                {
+                    ProductoId = 10,
+                    CategoriaId = 3,
+                    Nombre = "Torta de Frambuesa",
+                    Precio = 800
+                },
+                new Producto()
+                {
                     ProductoId = 4,
                     CategoriaId = 4,
                     Nombre = "Hamburguesa",
                     Precio = 0
+                },
+                new Producto()
+                {
+                    ProductoId = 11,
+                    CategoriaId = 2,
+                    Nombre = "Muzzarela",
+                    Precio = 700
+                },
+                new Producto()
+                {
+                    ProductoId = 12,
+                    CategoriaId = 2,
+                    Nombre = "Especial",
+                    Precio = 900
+                },
+                new Producto()
+                {
+                    ProductoId = 13,
+                    CategoriaId = 2,
+                    Nombre = "JamonYQueso",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 14,
+                    CategoriaId = 2,
+                    Nombre = "Napolitana",
+                    Precio = 900
+                },
+                new Producto()
+                {
+                    ProductoId = 15,
+                    CategoriaId = 2,
+                    Nombre = "4 Quesos",
+                    Precio = 950
+                },
+                new Producto()
+                {
+                    ProductoId = 16,
+                    CategoriaId = 2,
+                    Nombre = "Cebolla",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 17,
+                    CategoriaId = 2,
+                    Nombre = "Anchoa",
+                    Precio = 900
+                },
+                new Producto()
+                {
+                    ProductoId = 18,
+                    CategoriaId = 2,
+                    Nombre = "Anana",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 19,
+                    CategoriaId = 2,
+                    Nombre = "Huevo",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 20,
+                    CategoriaId = 2,
+                    Nombre = "Choclo",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 21,
+                    CategoriaId = 2,
+                    Nombre = "Milan",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 22,
+                    CategoriaId = 5,
+                    Nombre = "Carne",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 23,
+                    CategoriaId = 5,
+                    Nombre = "Choclo",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 24,
+                    CategoriaId = 5,
+                    Nombre = "Verdura",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 25,
+                    CategoriaId = 5,
+                    Nombre = "Pollo",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 26,
+                    CategoriaId = 5,
+                    Nombre = "JamonYQueso",
+                    Precio = 100
                 });
 
             modelBuilder.Entity<Adicional>()
