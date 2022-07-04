941edeb50102049db1245a604867ef697681d6e7
Matias <info-4@INFO4-PC5>
2022-07-04 09:34:55 -0300

    Retoques en PestañaHamburguesa__________

---
+++ BlazorApp1/Data/Migrations/20220704122341_fullProductos.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp1.Data.Migrations
+{
+    public partial class fullProductos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 6, 3, "Torta de vainilla", 500 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 7, 3, "Pastafrola", 700 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 8, 3, "Brownie c/mani", 750 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 9, 3, "Brownie c/mani", 800 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 10, 3, "Torta de Frambuesa", 800 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 11, 2, "Muzzarela", 700 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 12, 2, "Especial", 900 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 13, 2, "JamonYQueso", 850 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 14, 2, "Napolitana", 900 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 15, 2, "4 Quesos", 950 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 16, 2, "Cebolla", 850 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 17, 2, "Anchoa", 900 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 18, 2, "Anana", 850 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 19, 2, "Huevo", 850 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 20, 2, "Choclo", 850 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 21, 2, "Milan", 850 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 22, 5, "Carne", 100 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 23, 5, "Choclo", 100 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 24, 5, "Verdura", 100 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 25, 5, "Pollo", 100 });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "CategoriaId", "Nombre", "Precio" },
+                values: new object[] { 27, 5, "JamonYQueso", 100 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Productos",
+                keyColumn: "ProductoId",
+                keyValue: 27);
+        }
+    }
+}
