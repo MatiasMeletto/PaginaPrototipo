037f72316158e0b4013ac825f2cfd3326623a505
Matias <info-4@INFO4-PC5>
2022-07-15 10:10:23 -0300

    Agregado de productos, pestañabebida y modificaiocn de los values de las otras pestañas

--- BlazorApp1/Data/CompraContext.cs
+++ BlazorApp1/Data/CompraContext.cs
@@ -1,4 +1,4 @@
-﻿using BlazorApp1.Data.Models;
+﻿ using BlazorApp1.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp1.Data
@@ -13,15 +13,15 @@ namespace BlazorApp1.Data
         public DbSet<PedidoProducto> PedidoProductos { get; set; }
         public DbSet<CarritoProducto> CarritoProductos { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
-
+        public DbSet<PedidoProductoAdicional> pedidoProductoAdicionales { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite($"Data Source=compras.db");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Categoria>()
-                .HasData(new Categoria 
-                { 
+                .HasData(new Categoria
+                {
                     CategoriaId = 1,
                     Nombre = "Choripan"
                 },
@@ -61,10 +61,192 @@ namespace BlazorApp1.Data
                 },
                 new Producto()
                 {
+                    ProductoId = 3,
+                    CategoriaId = 3,
+                    Nombre = "Torta de chocolate",
+                    Precio = 500
+                },
+                new Producto()
+                {
+                    ProductoId = 5,
+                    CategoriaId = 3,
+                    Nombre = "Torta de zanahoria",
+                    Precio = 600
+                },
+                new Producto()
+                {
+                    ProductoId = 6,
+                    CategoriaId = 3,
+                    Nombre = "Torta de vainilla",
+                    Precio = 500
+                },
+                new Producto()
+                {
+                    ProductoId = 7,
+                    CategoriaId = 3,
+                    Nombre = "Pastafrola",
+                    Precio = 700
+                },
+                new Producto()
+                {
+                    ProductoId = 8,
+                    CategoriaId = 3,
+                    Nombre = "Brownie",
+                    Precio = 750
+                },
+                new Producto()
+                {
+                    ProductoId = 10,
+                    CategoriaId = 3,
+                    Nombre = "Torta de Frambuesa",
+                    Precio = 800
+                },
+                new Producto()
+                {
                     ProductoId = 4,
                     CategoriaId = 4,
                     Nombre = "Hamburguesa",
                     Precio = 0
+                },
+                new Producto()
+                {
+                    ProductoId = 11,
+                    CategoriaId = 2,
+                    Nombre = "Muzzarela",
+                    Precio = 700
+                },
+                new Producto()
+                {
+                    ProductoId = 12,
+                    CategoriaId = 2,
+                    Nombre = "Especial",
+                    Precio = 900
+                },
+                new Producto()
+                {
+                    ProductoId = 13,
+                    CategoriaId = 2,
+                    Nombre = "Jamon",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 14,
+                    CategoriaId = 2,
+                    Nombre = "Napolitana",
+                    Precio = 900
+                },
+                new Producto()
+                {
+                    ProductoId = 15,
+                    CategoriaId = 2,
+                    Nombre = "4 Quesos",
+                    Precio = 950
+                },
+                new Producto()
+                {
+                    ProductoId = 16,
+                    CategoriaId = 2,
+                    Nombre = "Cebolla",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 17,
+                    CategoriaId = 2,
+                    Nombre = "Anchoa",
+                    Precio = 900
+                },
+                new Producto()
+                {
+                    ProductoId = 18,
+                    CategoriaId = 2,
+                    Nombre = "Anana",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 19,
+                    CategoriaId = 2,
+                    Nombre = "Huevo",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 20,
+                    CategoriaId = 2,
+                    Nombre = "Choclo",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 21,
+                    CategoriaId = 2,
+                    Nombre = "Milan",
+                    Precio = 850
+                },
+                new Producto()
+                {
+                    ProductoId = 22,
+                    CategoriaId = 5,
+                    Nombre = "Carne",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 23,
+                    CategoriaId = 5,
+                    Nombre = "Choclo",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 24,
+                    CategoriaId = 5,
+                    Nombre = "Verdura",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 25,
+                    CategoriaId = 5,
+                    Nombre = "Pollo",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 27,
+                    CategoriaId = 5,
+                    Nombre = "JamonYQueso",
+                    Precio = 100
+                },
+                new Producto()
+                {
+                    ProductoId = 28,
+                    CategoriaId = 6,
+                    Nombre = "Coca-Cola",
+                    Precio = 170
+                },
+                new Producto()
+                {
+                    ProductoId = 29,
+                    CategoriaId = 6,
+                    Nombre = "Pepsi",
+                    Precio = 150
+                },
+                new Producto()
+                {
+                    ProductoId = 30,
+                    CategoriaId = 6,
+                    Nombre = "Sprite",
+                    Precio = 170
+                },
+                new Producto()
+                {
+                    ProductoId = 31,
+                    CategoriaId = 6,
+                    Nombre = "Fanta",
+                    Precio = 150
                 });
 
             modelBuilder.Entity<Adicional>()
@@ -117,7 +299,7 @@ namespace BlazorApp1.Data
                     Nombre = "Huevo",
                     Precio = 68,
                     ProductoId = 4
-                }, 
+                },
                 new Adicional()
                 {
                     AdicionalId = 8,
@@ -131,7 +313,7 @@ namespace BlazorApp1.Data
                     Nombre = "Cheddar",
                     Precio = 49,
                     ProductoId = 4
-                }, 
+                },
                 new Adicional()
                 {
                     AdicionalId = 10,
@@ -191,9 +373,16 @@ namespace BlazorApp1.Data
                 });
 
             modelBuilder.Entity<Carrito>()
-                .HasData(new Carrito() 
+                .HasData(new Carrito()
                 {
                     CarritoId = 1,
+                    UsuarioId = 1
+                });
+
+            modelBuilder.Entity<Pedido>()
+                .HasData(new Pedido()
+                {
+                    PedidoId = 1,
                     UsuarioId = 1
                 });
         }
